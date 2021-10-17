1369446bed73e99dbc97bf7982e2aad6c32fc375
DESKTOP-B87BICT\Глеб <glebka.dubilevskuy@yandex.ru>
2021-10-17 21:51:23 +0300

    breeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed

--- assets/todo.js
+++ assets/todo.js
@@ -1,3 +1,43 @@
-let myList = document.getElementsByTagName('LI');
-let i;
+function createTag(tagName, text) {
+    let $el = document.createElement(tagName);
+    $el.innerText = text;
+    return $el;
+}
 
+function initTodoList() {
+    let $todo = document.getElementById('todo');
+
+    let $addBtn = $todo.querySelector('.add-btn');
+    let $items = $todo.querySelector('.items');
+    let $input = $todo.querySelector('input');
+
+    $addBtn.addEventListener('click', function (e) {
+        e.stopPropagation();
+
+        if ($input.value === '') {
+            alert('You must write something!!!!');
+            return;
+        }
+
+        let $li = createTag('li', $input.value);
+
+        let $del = createTag('button', 'delete');
+        $del.addEventListener('click', function (e) {
+            e.stopPropagation();
+            $li.remove();
+        });
+
+        button.className = "delete"
+
+        $li.appendChild($del);
+
+        $items.appendChild($li);
+
+        $input.value = '';
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    initTodoList();
+});
