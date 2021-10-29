e702d4569c906e9965725c4bca24b1bc5f2e6dea
DESKTOP-B87BICT\Глеб <glebka.dubilevskuy@yandex.ru>
2021-10-29 18:22:36 +0300

    I dont know howdothis

--- assets/todo.js
+++ assets/todo.js
@@ -7,20 +7,35 @@ function createTag(tagName, html, className) {
 
 function initTodoList() {
     let $todo = document.getElementById('todo');
-
+    
     let $addBtn = $todo.querySelector('.add-btn');
     let $items = $todo.querySelector('.items');
-    let $input = $todo.querySelector('input');
+    var $input = $todo.querySelector('input');
 
     $addBtn.addEventListener('click', function (e) {
         e.stopPropagation();
-
+        
         if ($input.value === '') {
             alert('You must write something!!!!');
             return;
         }
+/* Крч чел делает вот это, 'nj e,bdftn ryjgre addBtn .Я понял это как сохранять последний ли который был написан(Он сохраняеться в аппликатион в локал сторедже). Он сохраняеться если убрать все это и 34 строка
+        let tdMask = 'tdL_';
+        $todo.children().each(function(index , ef){            (и в целом вот эта конструкция не понятна. По сути тут я пробегаюсь по каждому елементу ли
+            var jefId = $(ef).attr('data-itemid').slice(4);                   присваеваю ему атрибут  и убираю 4 символа('tdL_')
+            if(jefId > nId)                                                
+            nId = jefId;                                                                                                )
+        });
 
-        let $li = createTag('li', `<div class="item-text">${$input.value}</div>`, 'todo-item');
+        nId++;
+        var nId = 0 ;
+        attr('data-itemid' , tdMask+nId);
+
+        localStorage.setItem(tdMask+nId , $input.value); вот тут поменять tdmask+ nID на просто '' любой ключ 
+        но как сделать так что бы он сохранял все не понятно. мне каежться что я куда то не туда поставил 32 строку 
+*/
+      
+        var $li = createTag('li', `<div class="item-text">${$input.value}</div>`, 'todo-item');
 
         let $done = createTag('div', '<img src="./assets/img/done.svg" alt="Done">', 'done');
 
@@ -43,6 +58,8 @@ function initTodoList() {
 
         $input.value = '';
     });
+
+
 }
 
 let song = {
@@ -82,6 +99,8 @@ console.log(localStorage.getItem('number'));
 localStorage.setItem('number', myNumber);
 
 localStorage.setItem('song' , song);
+
+localStorage.setItem('song2' , JSON.stringify(song2));
 
 // тест комент//
 // test comment 2
